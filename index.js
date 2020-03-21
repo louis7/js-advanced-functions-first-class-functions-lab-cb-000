b4a2fc5953033d7dc1ca2fbc75d8c5d84886eb0b
louis baptiste <louisvis@ymail.com>
2020-03-21 03:21:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // Code your solution in this file!
 const returnFirstTwoDrivers = function( array ) {
   let first_two = array.slice(0,2)
-  return first_two
+  return first_two;
 };
 
 const returnLastTwoDrivers = function( array ) {
   let last_two = array.slice(array.lenght-1,array.lenght-2)
-  return last_two
+  return last_two;
 };
