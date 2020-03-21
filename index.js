0079cefcaecf4f65d73b94d0c462d1c7252b7acf
louis baptiste <louisvis@ymail.com>
2020-03-21 03:20:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ const returnFirstTwoDrivers = function( array ) {
   let first_two = array.slice(0,2)
   return first_two
 };
+
+const returnLastTwoDrivers = function( array ) {
+  let last_two = array.slice(array.lenght-1,array.lenght-2)
+  return last_two
+};
